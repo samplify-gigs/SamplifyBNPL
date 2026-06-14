8f84b1858aec5f8aaa3c5b3ba11f64c9c69adac2
samplify-gigs <samplifygigs@gmail.com>
2026-06-14 14:58:08 +0100

    set up merchant login pages

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "@/components/Navbar/NavBar";
+import HomeBody from "@/components/home/HomeBody";
 
 export default function Home() {
   return (
-    <main>
-      <header >
-      <NavBar/>
+    <main className="h-full pt-10 ">
+      <header className="w-full ">
+        <NavBar />
       </header>
+      <HomeBody />
     </main>
   );
 }
