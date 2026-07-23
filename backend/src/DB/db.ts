7d553da1646a5e351cb4aa057807e8026624d4c8
Samuel Jacobs <samplifygigs@gmail.com>
2026-07-23 19:26:52 +0100

    Update db.ts
    test to check if databse is connected

--- backend/src/DB/db.ts
+++ backend/src/DB/db.ts
@@ -6,3 +6,17 @@ export const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
+
+(async () => {
+  try {
+    const client = await pool.connect();
+    console.log("✅ Connected to PostgreSQL");
+
+    const result = await client.query("SELECT NOW()");
+    console.log(result.rows[0]);
+
+    client.release();
+  } catch (err) {
+    console.error("❌ Database connection failed:", err);
+  }
+})();
