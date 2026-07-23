a3babcdbfd66cccd4051916ae1884db6118a1b10
Samuel Jacobs <samplifygigs@gmail.com>
2026-07-23 19:18:55 +0100

    Update db.ts

--- backend/src/DB/db.ts
+++ backend/src/DB/db.ts
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  password: "bisolaluv",
-  user: "postgres",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
